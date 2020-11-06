2d428bc0b115bdbbb175db344ef2f1a916dc819e
Fen Pei <44687498+Fengithub@users.noreply.github.com>
2020-11-05 20:45:09 -0500

    Update cv_mf_benchmark.py

--- code/cv_mf_benchmark.py
+++ code/cv_mf_benchmark.py
@@ -39,41 +39,8 @@ def run_cv_scan():
         df = concat_files(all_auc_file, cur_auc_file, col_names, target_name)
         save_mean_file(df, col_names, target_name, mean_file)
 
-def evaluate_top_n():
-    binary = True
-    org = 'human'
-    data_source = 'benchmark'
-    model_type = 'symLMF'
-    n_folds = 5
-    top_n = 10
-
-    param = {'epochs': 20, 'batch_size': 64, 'k': 40, 'l_p': 1e-4, 'c': 1}
-
-    folder = '../' + org + '/' + model_type + '_result/selected/'
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    data_file = '../' + org + '/pros_AB.txt'
-    outfile = folder + 'mean_prec_recall@' + str(top_n) + '.txt'
-    n_pros, X_data, y_val = parse_data(data_file, binary, data_source)
-    skf_list = split_data(n_folds, X_data, y_val)
-    mean_prec_recall_res = np.zeros((n_folds, 2))
-    for fold_idx in range(n_folds):
-        model_output_file = folder + 'fold' + str(fold_idx) + '_model'
-        top_pair_out_file = folder + 'fold' + str(fold_idx) + '_top' + str(top_n) + '.txt'
-        X_train, y_train, X_test, y_test = get_train_test_data(fold_idx, skf_list, X_data, y_val)
-        mean_prec_recall_res[fold_idx, :] = cv2_evaluate(n_pros, X_train, y_train, X_test, y_test, param, model_type, model_output_file, top_n, top_pair_out_file)
-
-    mean_prec_recall = np.mean(mean_prec_recall_res, axis = 0)
-    std_prec_recall = np.std(mean_prec_recall_res, axis = 0)
-
-    outline = [[mean_prec_recall[0], std_prec_recall[0]], [mean_prec_recall[1], std_prec_recall[1]]]
-    outline = np.array(outline)
-    np.savetxt(outfile, outline, fmt = '%.4f')
-
 if __name__ == '__main__':
-    # run_cv_scan()
-    evaluate_top_n()
+    run_cv_scan()
 
 
 
