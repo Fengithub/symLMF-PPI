81a9a0b8d2e1bd2350937e4d48ee426ab230225b
Fen Pei <44687498+Fengithub@users.noreply.github.com>
2021-02-18 22:59:23 -0500

    Update cv_cnn.py

--- cnn/cv_cnn.py
+++ cnn/cv_cnn.py
@@ -1,9 +1,11 @@
 """
-Reference:Yao, Y.; Du, X.; Diao, Y.; Zhu, H., An integration of deep learning with feature embedding 
-for protein-protein interaction prediction. PeerJ 2019, 7, e7126.
+Reference: 
+Wang, Y.; You, Z.-H.; Yang, S.; Li, X.; Jiang, T.-H.; Zhou, X., A High Efficient Biological Language Model 
+for Predicting Protein⁻Protein Interactions. Cells 2019, 8, 122.
 
-code modified based on the originial code available on Github: https://github.com/xal2019/DeepFE-PPI
+code modified based on the originial code available at: https://figshare.com/s/b35a2d3bf442a6f15b6e
 """
+
 from gensim.models import Word2Vec
 from keras.models import Model
 from keras.models import load_model
